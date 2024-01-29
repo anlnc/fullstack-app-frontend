65a76701187203a000d2701a2ddfd61a0cb0a815
An Le <anle@BZ-MAC-015.local>
2024-01-29 11:33:46 +0700

    Update API URL in client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const api_url = process.env.SERVER_URL;
+const api_url = process.env.REACT_APP_API_URL;
 console.log("process.env", process.env);
 console.log("api_url", api_url);
 
